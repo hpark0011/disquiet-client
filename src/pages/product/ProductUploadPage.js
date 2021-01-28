941565f1322cb0debc8ab399b1ce1b35bc1e6e03
Hyunsol Park <hpark0011@gmail.com>
2021-01-28 15:25:46 +0900

    add top margin on product upload page body

--- src/pages/product/ProductUploadPage.js
+++ src/pages/product/ProductUploadPage.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
-import styled, { ThemeProvider } from "styled-components";
-import theme from "../../theme";
+import React, { useState } from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+import theme from '../../theme';
 
 function ProductUploadPage() {
-  const [text, setText] = useState("");
+  const [text, setText] = useState('');
 
   const handleSubmit = () => {};
 
@@ -13,37 +13,37 @@ function ProductUploadPage() {
         <Container>
           <p>프로덕트 공유하기</p>
           <FormWrapper onSubmit={handleSubmit}>
-            <FormLabel type="label">
+            <FormLabel type='label'>
               링크<Span>*</Span>
             </FormLabel>
             <FormInput
-              type="text"
-              placeholder="URL (예시 - https://www.disquiet.tech)"
+              type='text'
+              placeholder='URL (예시 - https://www.disquiet.tech)'
             />
-            <FormLabel type="label">
+            <FormLabel type='label'>
               프로덕트 이름<Span>*</Span>
             </FormLabel>
-            <FormInput type="text" placeholder="예시 - 디스콰이엇" />
-            <FormLabel type="label">
+            <FormInput type='text' placeholder='예시 - 디스콰이엇' />
+            <FormLabel type='label'>
               프로덕트에 대한 간단한 설명<Span>*</Span>
             </FormLabel>
             <FormInput
-              type="text"
-              placeholder="예시 - IT프로덕트 디스커버리 사이트"
+              type='text'
+              placeholder='예시 - IT프로덕트 디스커버리 사이트'
             />
-            <FormLabel type="label">
+            <FormLabel type='label'>
               카테고리<Span>*</Span>
             </FormLabel>
-            <FormInput type="text" />
-            <FormLabel type="label" placeholder="">
+            <FormInput type='text' />
+            <FormLabel type='label' placeholder=''>
               메이커<Span>*</Span>
             </FormLabel>
-            <FormInput type="text" />
-            <FormLabel type="label" placeholder="">
+            <FormInput type='text' />
+            <FormLabel type='label' placeholder=''>
               추천이유<Span>*</Span>
             </FormLabel>
-            <TextArea type="textarea" />
-            <FormSubmitBtn type="submit" value="프로덕트 올리기" />
+            <TextArea type='textarea' />
+            <FormSubmitBtn type='submit' value='프로덕트 올리기' />
           </FormWrapper>
         </Container>
       </ThemeProvider>
@@ -67,8 +67,9 @@ const Container = styled.div`
   p {
     text-align: center;
     font-size: ${({ theme }) => theme.fontSizes.xl};
-    font-weight: 400;
+    font-weight: 500;
     margin-bottom: 1.25rem;
+    margin-top: 3rem;
   }
 `;
 
